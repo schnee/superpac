3cd166bd1560d088691e4db3bc14fc3dada6b131
bschneeman <bschneeman@homeaway.com>
2014-02-15 08:18:18 -0600

    one file to handle both support and oppose

---
+++ orientation-by-state.R
@@ -0,0 +1,52 @@
+#!/usr/bin/Rscript
+
+require(ggplot2)
+require(plyr)
+require(maps)
+require(grid)
+require(mapproj)
+require(scales)
+
+pac <- read.csv("./committee_summary.csv", header=TRUE)
+p <- pac
+names(p)[12] <- "receipts"
+
+superpac.spend <- ddply(p, "state", summarize, sd = sum(IEs.support.dems, na.rm=TRUE), 
+            sr=sum(IEs.support.reps, na.rm=T),
+            od = sum(IEs.oppose.dems, na.rm=T),
+            or = sum(IEs.oppose.reps, na.rm=T))
+
+states_map <- map_data("state")
+
+superpac.spend$statelc <- tolower(state.name[match(superpac.spend$state, state.abb)])
+
+superpac.spend$statelc[9] <- "district of columbia"
+
+superpac.spend$support <- (superpac.spend$sr-superpac.spend$sd)/(superpac.spend$sd+superpac.spend$sr)
+superpac.spend$oppose <- (superpac.spend$od-superpac.spend$or)/(superpac.spend$od+superpac.spend$or)
+
+clean_theme <- theme(axis.title = element_blank(), axis.text=element_blank(), panel.background=element_blank(), panel.grid=element_blank(), axis.ticks.length = unit(0, "cm"), complete=TRUE)
+
+ggplot(superpac.spend, aes(map_id = statelc, fill=support)) + 
+  geom_map(map=states_map) + 
+  expand_limits(x=states_map$long, y=states_map$lat) + 
+  coord_map("polyconic") + 
+  scale_fill_gradient2(high=muted("red"), 
+                       low=muted("blue"), 
+                       mid="white",
+                       labels=c("Democratic","Mostly Democratic", "Even", "Mostly Republican", "Republican")) + 
+  labs(fill="Supporting\nOrientation") + 
+  ggtitle("Statewise Spending Orientation\n[Supporting]") +
+  clean_theme
+
+ggplot(superpac.spend, aes(map_id = statelc, fill=oppose)) + 
+  geom_map(map=states_map) + 
+  expand_limits(x=states_map$long, y=states_map$lat) + 
+  coord_map("polyconic") + 
+  scale_fill_gradient2(high=muted("red"), 
+                       low=muted("blue"), 
+                       mid="white",
+                       labels=c("Democratic","Mostly Democratic", "Even", "Mostly Republican", "Republican")) + 
+  labs(fill="Opposing\nOrientation") + 
+  ggtitle("Statewise Spending Orientation\n[Opposing]") +
+  clean_theme
--- support-by-state.R
+++ support-by-state.R
@@ -25,12 +25,18 @@ sp <- sd
 
 sp$sr <- sr$sr
 
-#names(sp)[2] <- "sd"
-
-pscore <- (sp$sr-sp$sd)/(sp$sd+sp$sr)
-
-qpscore <- quantile(pscore, c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0), na.rm=TRUE)
+sp$pscore <- (sp$sr-sp$sd)/(sp$sd+sp$sr)
 
 clean_theme <- theme(axis.title = element_blank(), axis.text=element_blank(), panel.background=element_blank(), panel.grid=element_blank(), axis.ticks.length = unit(0, "cm"), complete=TRUE)
 
-ggplot(sp, aes(map_id = statelc, fill=(sr-sd)/(sd+sr)))+ geom_map(map=states_map) + expand_limits(x=states_map$long, y=states_map$lat)+coord_map("polyconic") + scale_fill_gradient2(high=muted("red"), low=muted("blue"), mid="white",labels=c("Democratic","Mostly Democratic", "Even", "Mostly Republican", "Republican")) + labs(fill="Supporting\nOrientation") + ggtitle("Statewise Spending Orientation\n[Supporting]") +clean_theme
+ggplot(sp, aes(map_id = statelc, fill=pscore)) + 
+  geom_map(map=states_map) + 
+  expand_limits(x=states_map$long, y=states_map$lat) + 
+  coord_map("polyconic") + 
+  scale_fill_gradient2(high=muted("red"), 
+                       low=muted("blue"), 
+                       mid="white",
+                       labels=c("Democratic","Mostly Democratic", "Even", "Mostly Republican", "Republican")) + 
+  labs(fill="Supporting\nOrientation") + 
+  ggtitle("Statewise Spending Orientation\n[Supporting]") +
+  clean_theme
